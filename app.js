33f42bf9ec4e26a1e7f23233dbba1b4f6cc00b62
wjb <wjbryson@gmail.com>
2020-07-31 13:27:13 -0400

    committing

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.get('/dogs/:id', (req, res) => {
+  const dogs = [{
+    id: 1,
+    name: 'Roger'
+  }]
 
+  const dog = dogs.filter(dog => dog.id === req.params.id)
+  if (dog.length) {
+    res.json(dog[0])
+  } else {
+    res.status(404).send('File not found')
+  }
+})
 
 app.listen(3000, () => console.log('Server ready'));
